0db2779590b6fa7accd49ddb2a3cd85a63a3de23
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-01-21 16:29:52 -0600

    MVP met

--- react-redux-async/src/redux/actions.js
+++ react-redux-async/src/redux/actions.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 export const fetchData=()=>{
     return(dispatch=>{
         dispatch({type:"FETCH_DATA_START"});
-        console.log("from the action")
-            axios.get("https://api.kanye.rest")//add a url here
+            axios.get("https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49 ")//add a url here
                 .then((res)=>{
-                    console.log(res.data.quote)
-                    dispatch({type:"FETCH_DATA_SUCCESS",payload:res.data.quote})
+                    // console.log(res.data)
+                    dispatch({type:"FETCH_DATA_SUCCESS",payload:res.data})
                 })
                 .catch((err)=>{
                     dispatch({type:"FETCH_DATA_FAILURE",payload:err})
--- react-redux-async/src/redux/reducer.js
+++ react-redux-async/src/redux/reducer.js
@@ -3,22 +3,28 @@
 
 const initialState={
     data:"",
+    isLoading:"",
     error:""
 };
 
 export const reducer=(state=initialState,action)=>{
     switch(action.type){
         case("FETCH_DATA_START"):
-        // console.log("from the reducer")
-            return state;
-        case("FETCH_DATA_SUCCESS"):
             return({
-                data:action.payload
+                ...state,
+                isLoading:"Loading the data..."
+            });
+        case("FETCH_DATA_SUCCESS"):
+            console.log(state.data.title)
+            return({
+                data:action.payload,
+                isLoading:""
             })
         case("FETCH_DATA_FAILURE"):
             return ({
                 ...state,
-                error:action.payload
+                isLoading:"",
+                error:"oops ya broke me"
             })
         default:
             return state;
